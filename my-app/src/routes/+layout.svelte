5ce7d3190a068fb64ac70d5fa0e6345a861ff702
kongmenglor <kongmeng.k.lor@gmail.com>
2024-02-20 14:17:37 -0700

    first commit

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+	import "../app.pcss";
+    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    import '../app.pcss';
+    import './styles.css';
+    import { page } from '$app/stores';
+
+
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<nav class="w-full">
+		<!-- Insert the list: -->
+			<ul class="horizontal">
+				<!-- Change this out for home button when possible -->
+				<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a style="color:white;" class="btn variant-filled-primary" href="/">Home</a>
+				</li>
+				
+				<li aria-current={$page.url.pathname === '/upload' ? 'page' : undefined}>
+					<a style="color:white;" class="btn variant-filled-primary" href="/upload">Upload Notes</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- --- -->
+	</svelte:fragment>
+</AppShell>
+
+<div class="app">
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	ul {
+		background-color: #333;
+		list-style: none;
+		padding: 0;
+		display: flex;
+
+	}
+
+</style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<script lang='ts'>
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+
+</script>
+
+<svelte:head>
+	<title>Unnamed Notes App</title>
+	<meta name="description" content="Information To Enter" />
+</svelte:head>
+
+<div>
+	<h1>Unnamed Notes App</h1>
+</div>
+
+<div>
+
+	<form class="max-w-2xl mx-auto p-8 bg-white shadow-md rounded-md grid grid-cols-2">
+		<div class="mb-4">
+			<label for="teacher_name" class="block text-gray-700 font-bold mb-2">Teacher Name: </label>
+			<input type="text" id="teacher_name" name="teacher_name" class="w-62 p-2 border rounded-md">
+		</div>
+		<div class="mb-4">
+			<label for="class" class="block text-gray-700 font-bold mb-2">Class Number: </label>
+			<div class="mb-4 grid grid-cols-2">
+				<div>
+					<input type="text" id="class_dept" name="class_dept" class="w-36 p-2 border rounded-md">
+				</div>
+				<div>
+					<input type="text" id="class_number" name="class_number" class="w-36 p-2 border rounded-md">
+				</div>
+			</div>
+		</div>
+
+		<!-- Submit Button -->
+		<div class="mt-6 p-8 grid grid-cols-1 gap-2">
+			<button type="submit" class="w-full bg-blue-500 text-black p-2 rounded-md">Search</button>
+		</div>
+	</form>
+	<!-- {#if searching}
+	<div>
+		<ProgressBar meter="bg-sky-500" track=" bg-sky-500/30" class="w-full" value={undefined} />
+	</div>
+	{/if}
+
+	{#if searched}
+		<h1>Information Fields to Update: </h1>
+		<form on:submit|preventDefault={updateSchema} class='max-w-xl mx-auto p-8 bg-white shadow-md rounded-md' id='reportGen'>
+			<Template dataShown={dataToShow}/>
+			<div class='mt-6 p-8 grid grid-cols-1 gap-2'>
+				<button type='submit' class='w-full bg-red-500 text-white p-2 rounded-md'>Save</button>
+			</div>
+		</form>
+			
+	{/if} -->
+</div>
+
+<style>
+	h1 {
+		width: 100%;
+		color: black;
+		text-align: center;
+		padding: 4rem;
+	}
+
+</style>
