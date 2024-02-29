59d4f71b33aeffea32ab6602981b90d9a449e598
kongmenglor <kongmeng.k.lor@gmail.com>
2024-02-28 21:35:11 -0700

    Got file upload and download working.

--- my-app/src/routes/upload/+page.svelte
+++ my-app/src/routes/upload/+page.svelte
@@ -1,8 +1,14 @@
 <script lang='ts'>
 	import { Autocomplete, popup, FileDropzone, ProgressBar } from '@skeletonlabs/skeleton';
 	import type { AutocompleteOption, PopupSettings  } from '@skeletonlabs/skeleton';
+	import { supabase } from '$lib/images/supabaseClient';
 
 	//Temp values to fill out later
+
+	let uploading = false;
+	let uploaded = false;
+	let input_file: FileList;
+
 	let dept_input = '';
 	let number_input = '';
 	let teacher_input = '';
@@ -49,6 +55,29 @@
 		placement: 'bottom',
 	};
 
+	async function placeInBucket() {
+		const {data, error} = await supabase.storage.from('notes').upload('/' + input_file[0].name, input_file[0]);
+	}
+
+	async function handleSubmit(){
+		//comment this out for prod
+		console.log(input_file);
+		uploading = true;
+		//put it into bucket
+			setTimeout(async () => {
+				placeInBucket();
+				//put into database
+				const { data } = await supabase.schema('all_info').from('notes').insert({department: dept_input, class_number: number_input, professor: teacher_input, document_name: input_file[0].name, upload_date: new Date(), current_rating: 5});
+			}
+			, 5000);
+
+		setTimeout(() => {
+			uploading = false;
+			uploaded = true;
+		}
+		, 5000);
+	}
+
 
 
 </script>
@@ -58,11 +87,24 @@
 	<meta name="description" content="Information To Enter" />
 </svelte:head>
 
-<FileDropzone name="file_in" accept=".docx, .pdf, .png" class="h-80 text-4xl max-w-xl mx-auto p-8 bg-white shadow-md rounded-md p-4">
+
+{#if uploaded}
+<aside class="alert max-w-xs mx-auto h-22 p-4 bg-gray-300 shadow-md rounded-md space-x-4">
+	<div class='alert-message'>
+		<span class="block text-gray-700 font-bold mb-2 text-center p-2">Notes uploaded successfully!</span>
+	</div>
+	<div class='alert-actions'>
+		<button class="btn w-full bg-red-400 text-black p-2 rounded-md text-center text-sm" on:click={() => {uploaded = false;}}>Dismiss</button>
+	</div>
+</aside>
+{/if}
+
+
+<FileDropzone bind:files={input_file} name="file_in" accept=".docx, .pdf, .png" class="h-80 text-4xl max-w-xl mx-auto p-8 bg-white shadow-md rounded-md p-4">
 	<svelte:fragment slot="message">Drag and Drop file... <br> (png, docx, pdf)</svelte:fragment>
 </FileDropzone>
 
-<form class="max-w-xl mx-auto p-8 shadow-md rounded-md">
+<form on:submit|preventDefault={handleSubmit} class="max-w-xl mx-auto p-8 shadow-md rounded-md">
 	<div class="mb-4 grid grid-cols-2 space-x-4">
 		<label for="class" class="bg-gray-300 block text-gray-700 mb-2 text-center w-30 p-2 border rounded-md">Class Number: </label>
 		<div class="grid grid-cols-2 space-x-4">
@@ -122,19 +164,9 @@
 		<button type="submit" class="w-full bg-gray-300 text-black p-2 rounded-md">Upload!</button>
 	</div>
 </form>
-<!-- {#if searching}
+
+{#if uploading}
 <div>
-	<ProgressBar meter="bg-sky-500" track=" bg-sky-500/30" class="w-full" value={undefined} />
+	<ProgressBar meter="bg-sky-500" track=" bg-sky-500/30" class="w-10/12 mx-auto" value={undefined} />
 </div>
 {/if}
-
-{#if searched}
-	<h1>Information Fields to Update: </h1>
-	<form on:submit|preventDefault={updateSchema} class='max-w-xl mx-auto p-8 bg-white shadow-md rounded-md' id='reportGen'>
-		<Template dataShown={dataToShow}/>
-		<div class='mt-6 p-8 grid grid-cols-1 gap-2'>
-			<button type='submit' class='w-full bg-red-500 text-white p-2 rounded-md'>Save</button>
-		</div>
-	</form>
-		
-{/if} -->
