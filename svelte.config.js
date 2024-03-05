873767555e803aceb6261ccd894419e33f781b46
kongmenglor <kongmeng.k.lor@gmail.com>
2024-03-05 13:26:10 -0700

    Another push for deploy.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "svelte-adapter-bun";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,12 +7,10 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    paths: {
+          base: process.env.NODE_ENV === "production" ? "/kongmenglor.github.io" : "",
+      },
   },
-  prerender: {
-    onError: () => {
-      return "continue";
-    }
-  }
 };
 
 export default config;
